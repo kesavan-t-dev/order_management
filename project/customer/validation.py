45c172a57f936967ca96924d2a696f4fd545ab5e
Pragatheeswaran-sv <pragatheeswaran.s@mitrahsoft.in>
2026-03-02 17:12:32 +0530

    Completed customer endpoints

--- project/customer/validation.py
+++ project/customer/validation.py
@@ -1,11 +1,20 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict
 from sqlalchemy.dialects.postgresql import UUID
-from typing import List, Optional
+from typing import List
 import uuid
 
-class Customer_serializer(BaseModel):
-    
+class Create_Customer(BaseModel):
     id : UUID = uuid.uuid4
+    name : str
+    email : str
+    number : str
+    address : str
+    city : str
+    zip : str 
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+class Customer_serializer(BaseModel):
     name : str
     email : str
     number : str
@@ -14,35 +23,17 @@ class Customer_serializer(BaseModel):
     zip : str
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    
-    
+   
+class Customer_response(BaseModel):
+    message: str 
+    properties: List[Customer_serializer] | None
+    status_code: int
+ 
 class Customer_update_serializer(BaseModel):
-
     name : str | None = None
     email : str | None = None
     number : str | None = None
     address : str | None = None
     city : str | None = None
     zip : str | None = None
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    
-
-class Customer_response(BaseModel):
- 
-    message: str 
-    properties: List[Customer_serializer] | None
-    status_code: int
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    
-
-class Create_Customer(BaseModel):
-    
-    id : UUID = uuid.uuid4
-    name : str
-    email : str
-    number : str
-    address : str
-    city : str
-    zip : str 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
     
