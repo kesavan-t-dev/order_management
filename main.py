ffc7eee7821515c41beffb550819a1c299b6f236
Pragatheeswaran-sv <pragatheeswaran.s@mitrahsoft.in>
2026-03-02 21:33:58 +0530

    customer endpoints

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # main.py
 from fastapi import FastAPI
 from project.customer.api import * 
-
+from project.olap.dim_customer.api import *
 # Create a FastAPI "instance"
 # app = FastAPI()
 app = FastAPI(debug=True)
 
 app.include_router(customer_route)
+# app.include_router(report_route)
 # app.include_router(product_route)
 # app.include_router(order_route)
 
---
+++ project/olap/dim_customer/api.py
@@ -0,0 +1,16 @@
+# from fastapi import APIRouter, Depends
+# from sqlalchemy.orm import Session
+# from .apscheduler import *
+# from project.customer.validation import *
+# # from db import get_db
+
+# report_route = APIRouter(
+#     prefix= '/export',
+#     tags= ['export'],
+#     responses={404: {"description": "Not found"}}
+# )
+
+# @report_route.get('/data')
+# def export_data():
+#     return export()
+
