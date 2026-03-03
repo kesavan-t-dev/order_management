5cd357330ef1f8b2be5468b7057775566c2b059b
Pragatheeswaran-sv <pragatheeswaran.s@mitrahsoft.in>
2026-03-03 11:30:59 +0530

    commit

--- project/olap/dim_customer/service.py
+++ project/olap/dim_customer/service.py
@@ -1,19 +1,18 @@
 from .models import *
 
-def export():
-    name = 'hello'
-    print(name)
+def create():
+    
     # cur.execute('select * from customer')
-    create = '''CREATE TABLE dim_Customer(
-        id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-        CustomerID INT UNIQUEIDENTIFIER,
-        Name VARCHAR(50) NOT NULL
-    );'''
+    create = '''create table customer(
+                    id UNIQUEIDENTIFIER DEFAULT NEWID(),
+                    customer_id UNIQUEIDENTIFIER,
+                    name varchar(max),
+                    number varchar(10));
+            '''
     cur.execute(create)
     db.commit()
-    print(cur)
-    db.commit()
-    print("Connection successful!")
+    # print(cur)
+    # db.commit()
 
 def dim_create(cus_id, name, number):
     cur.execute('insert into dbo.customer (customer_id, name, number) values (%s, %s, %s)', (cus_id, name, number))
