a39fe1a5c90d1a7c1e478009f5458aff833776aa
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2026-03-03 16:59:26 +0530

    export endpoint work correctly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from project.customer.api import * 
 from project.orders.api import router as order_router
+from project.olap.dim_order.export_csv import router as olap_router
 from project.customer.models import Customer
 from project.product.models import Product
 from project.orders.models import Order, Order_items
@@ -9,6 +10,7 @@ from project.orders.models import Order, Order_items
 app = FastAPI(debug=True)
 
 app.include_router(order_router, prefix="/order", tags=["Order"])
+app.include_router(olap_router, prefix="/olap", tags=["OLAP"])
 
 @app.get("/")
 def read_root():
